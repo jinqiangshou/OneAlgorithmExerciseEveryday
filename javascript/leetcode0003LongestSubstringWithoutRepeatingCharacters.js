2c07e24e91f9db304f920c6a5434e986c6373a69
Horst Xu <271021733@qq.com>
2015-04-02 14:08:31 +0800

    Update leetcode0003LongestSubstringWithoutRepeatingCharacters.js

--- javascript/leetcode0003LongestSubstringWithoutRepeatingCharacters.js
+++ javascript/leetcode0003LongestSubstringWithoutRepeatingCharacters.js
@@ -13,29 +13,29 @@
  */
 var lengthOfLongestSubstring = function(s) {
     if(typeof(s) !== 'string' || s.length === 0){
-            return 0;
-        } else if(s.length === 1){
-            return 1;
+        return 0;
+    } else if(s.length === 1){
+        return 1;
+    }
+    var hash = new Array(256);
+    for(var i=0; i<hash.length;i++){
+        hash[i] = 0;
+    }
+    var start = 0;
+    var end = 0;
+    var len=0;
+    while(end<s.length){
+        var num=s.charAt(end).charCodeAt();
+        hash[num]++;
+        if(hash[num] === 1){
+            len=(end-start+1>len)?(end-start+1):(len);   
+        }else{
+            do{
+                hash[s.charAt(start).charCodeAt()]--;
+                start++;
+            }while(start<end && hash[num] > 1);                
         }
-        var hash = new Array(256);
-        for(var i=0; i<hash.length;i++){
-            hash[i] = 0;
-        }
-        var start = 0;
-        var end = 0;
-        var len=0;
-        while(end<s.length){
-            var num=s.charAt(end).charCodeAt();
-            hash[num]++;
-            if(hash[num] === 1){
-                len=(end-start+1>len)?(end-start+1):(len);   
-            }else{
-                do{
-                    hash[s.charAt(start).charCodeAt()]--;
-                    start++;
-                }while(start<end && hash[num] > 1);                
-            }
-            end++;           
-        }
-        return len;
+        end++;           
+    }
+    return len;
 };
