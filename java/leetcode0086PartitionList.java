43fa3ae3b26b935ea60b52bf99f27adac56b6c8b
Horst Xu <271021733@qq.com>
2015-07-26 22:33:15 +0800

    Update leetcode0086PartitionList.java

--- java/leetcode0086PartitionList.java
+++ java/leetcode0086PartitionList.java
@@ -1,6 +1,6 @@
 /* **********************************Question***************************************
  * Given a linked list and a value x, partition it such that all nodes less than x come 
- *before nodes greater than or equal to x.
+ * before nodes greater than or equal to x.
  * You should preserve the original relative order of the nodes in each of the two partitions.
  * For example,
  * Given 1->4->3->2->5->2 and x = 3,
