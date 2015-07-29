e89ab9cd35a82d2f21c746ada815d412a11d69b5
Horst Xu <271021733@qq.com>
2015-07-29 14:00:08 +0800

    Create leetcode0089GrayCode.cpp

---
+++ c-cpp/leetcode0089GrayCode.cpp
@@ -0,0 +1,33 @@
+/*******************************Question********************************
+ * The gray code is a binary numeral system where two successive values differ in only one bit.
+ * Given a non-negative integer n representing the total number of bits in the code, 
+ * print the sequence of gray code. A gray code sequence must begin with 0.
+ * For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+ * 00 - 0
+ * 01 - 1
+ * 11 - 3
+ * 10 - 2
+ * Note:
+ * For a given n, a gray code sequence is not uniquely defined.
+ * For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
+ * For now, the judge is able to judge based on one instance of gray code sequence. Sorry about that.
+ ************************************************************************/
+
+
+/**
+ * @author Horst Xu
+ * @date 2015-07-29
+ **/
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+         vector<int> result;
+         int m = 1 << n;
+         for(int i = 0; i < m; i++)
+         {
+             result.push_back(i ^ (i >> 1));
+         }
+         return result;
+         
+    }
+};
