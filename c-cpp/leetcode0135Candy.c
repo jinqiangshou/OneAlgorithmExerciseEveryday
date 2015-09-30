5da17363a0d8887bebd42df0663634f564b16358
Horst Xu <271021733@qq.com>
2015-09-30 11:29:17 +0800

    Update leetcode0135Candy.c

--- c-cpp/leetcode0135Candy.c
+++ c-cpp/leetcode0135Candy.c
@@ -12,40 +12,40 @@
  * @date 2015-09-30
  **/
 int candy(int ratings[], int n) {
-    if(n==0){return 0;}
+    if(n==0){ return 0; }
 	
-	int total = 1;
-	int current = 1;
+    int total = 1;
+    int current = 1;
 	
-	int pos = 1;
-	while(pos < n){
-		if(ratings[pos] > ratings[pos-1]){
-			current ++;
-			total += current;
-			pos ++;
-		}else if(ratings[pos] == ratings[pos-1]){
-			current = 1;
-			total += current;
-			pos ++;
-		}else{ //ratings[pos] < ratings[pos-1]
-			int count = 0;
-			while(pos < n){
-				if(ratings[pos] >= ratings[pos-1]){
-					break;
-				}
-				pos ++;
-				count ++;
-			}
-			if(current > count){
-				total += count*(count+1)/2;
-				current = 1;
-			}else{
-				total -= current;
-				total += (count+1)*(count+2)/2;
-				current = 1;
-			}
-		}
-	}
+    int pos = 1;
+    while(pos < n){
+        if(ratings[pos] > ratings[pos-1]){
+            current ++;
+            total += current;
+            pos ++;
+        }else if(ratings[pos] == ratings[pos-1]){
+            current = 1;
+            total += current;
+            pos ++;
+        }else{ //ratings[pos] < ratings[pos-1]
+            int count = 0;
+            while(pos < n){
+                if(ratings[pos] >= ratings[pos-1]){
+                    break;
+                }
+                pos ++;
+                count ++;
+            }
+            if(current > count){
+                total += count*(count+1)/2;
+                current = 1;
+            }else{
+                total -= current;
+                total += (count+1)*(count+2)/2;
+                current = 1;
+            }
+        }
+    }
 	
-	return total;
+    return total;
 }
