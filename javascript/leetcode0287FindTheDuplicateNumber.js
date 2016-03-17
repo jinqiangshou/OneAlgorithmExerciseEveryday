789c2f877c0a5117e3fda9db8031bd58de5cc375
Horst Xu <271021733@qq.com>
2016-03-17 20:57:17 +0800

    Update leetcode0287FindTheDuplicateNumber.js

--- javascript/leetcode0287FindTheDuplicateNumber.js
+++ javascript/leetcode0287FindTheDuplicateNumber.js
@@ -6,7 +6,7 @@
  * Note:
  * You must not modify the array (assume the array is read only).
  * You must use only constant, O(1) extra space.
- * Your runtime complexity should be less than O(n2).
+ * Your runtime complexity should be less than O(n^2).
  * There is only one duplicate number in the array, but it could be repeated more than once.
  ********************************************************************************/
 
